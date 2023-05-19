2bf3763217f7b6a7dc7d12c19f8ee6cf75bbf311
Noname MissingNo <csabika013@hotmail.com>
2023-05-19 19:16:32 +0200

    Changed name of Application

--- src/main/java/com/example/demo/LibraryApplication.java
+++ src/main/java/com/example/demo/LibraryApplication.java
@@ -3,11 +3,13 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Class to provide an entry point for the music catalog webservice.
+ */
 @SpringBootApplication
-public class DemoApplication {
+public class LibraryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(LibraryApplication.class, args);
 	}
-
 }
