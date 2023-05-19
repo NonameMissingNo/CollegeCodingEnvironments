1d1a5af6132d7b738891d6aa36f69325075b4829
Noname MissingNo <csabika013@hotmail.com>
2023-05-19 19:13:27 +0200

    Changed controller

--- src/main/java/com/example/demo/web/controller/LibraryController.java
+++ src/main/java/com/example/demo/web/controller/LibraryController.java
@@ -1,19 +1,124 @@
 package com.example.demo.web.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.example.demo.data.model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.service.BookService;
+
 /**
- * Controller for the library.
+ * Controller for the music catalog.
  */
 @Controller
 @RequestMapping("/library")
 public class LibraryController {
 
+    private final BookService bookService;
 
     @Autowired
-    public LibraryController() {
+    public LibraryController(BookService bookService) {
+        this.bookService = bookService;
+    }
 
+    /**
+     * Shows the book editor screen.
+     *
+     * @param model the model object to store attributes
+     * @param id the id of the book to retrieve
+     * @return the name of the edit view to render
+     */
+    @GetMapping("/{id}")
+    public String getBookById(Model model, @PathVariable Long id) {
+        Optional<Book> optionalSong = bookService.retrieveBookById(id);
+        return optionalSong.map(song -> {
+            model.addAttribute("book", song);
+            return "library/edit";
+        }).orElseGet(() -> {
+            model.addAttribute("requestUri", "library/" + id);
+            return "library/error";
+        });
+    }
+
+    /**
+     * Shows the book list screen.
+     *
+     * @param model the model object to store attributes
+     * @return the name of the book list view to render
+     */
+    @GetMapping
+    public String getAllBooks(Model model) {
+        List<Book> allSongs = bookService.retrieveAllBooks();
+        model.addAttribute("books", allSongs);
+        return "library/list";
+    }
+    @GetMapping("/create")
+    public String createBook() {
+        return "library/create";
+    }
+
+    /**
+     * Creates a new book.
+     * Also navigates back to the editor screen.
+     *
+     * @param model the model object to store attributes
+     * @param book the book object to create
+     * @return the name of the edit view to render
+     */
+    @PostMapping("/create")
+    public String createBook(Model model, Book book) {
+        Book newBook = bookService.createBook(book);
+        model.addAttribute("book", newBook);
+        return "library/edit";
+    }
+
+    /**
+     * Updates an existing song.
+     * Also navigates back to the editor screen.
+     *
+     * @param model the model object to store attributes
+     * @param book the book object to update
+     * @return the name of the edit view to render
+     */
+    @PostMapping(value = "/update", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String updateBook(Model model, Book book) {
+        Book updatedBook = bookService.updateBook(book);
+        model.addAttribute("book", updatedBook);
+        return "library/edit";
+    }
+
+    /**
+     * Deletes a song by ID.
+     * Also navigates back to the song list screen.
+     *
+     * @param model the model object to store attributes
+     * @param id the id of the book to delete
+     * @return the name of the book list view to render
+     */
+    @GetMapping("/{id}/delete")
+    public String deleteBookById(Model model, @PathVariable Long id) {
+        bookService.deleteBookById(id);
+        List<Book> allBooks = bookService.retrieveAllBooks();
+        model.addAttribute("books", allBooks);
+        return "library/list";
+    }
+    @GetMapping("/{id}/rating")
+    public String rateBookById(Model model, @PathVariable Long id) {
+        Optional<Book> optionalSong = bookService.retrieveBookById(id);
+        return optionalSong.map(song -> {
+            model.addAttribute("book", song);
+            return "library/rating";
+        }).orElseGet(() -> {
+            model.addAttribute("requestUri", "library/" + id);
+            return "library/error";
+        });
     }
 }
--- src/main/java/com/example/demo/web/controller/LibraryRestController.java
+++ src/main/java/com/example/demo/web/controller/LibraryRestController.java
@@ -1,17 +1,88 @@
 package com.example.demo.web.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.example.demo.data.model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.BookService;
 
 /**
  * A REST controller for managing songs in the music catalog.
  */
 @RestController
-@RequestMapping("/api/v1/book")
+@RequestMapping("/api/v1/song")
 public class LibraryRestController {
 
+    private final BookService bookService;
+
     @Autowired
-    public LibraryRestController() {
+    public LibraryRestController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    /**
+     * Returns a song with the given id.
+     *
+     * @param id the id of the song to retrieve
+     * @return the song object
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getSongById(@PathVariable Long id) {
+        Optional<Book> song = bookService.retrieveBookById(id);
+        return song.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    /**
+     * Returns a list of all the songs in the catalog.
+     *
+     * @return the list of songs
+     */
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.retrieveAllBooks();
+    }
+
+    /**
+     * Creates a new song and returns it.
+     *
+     * @param song the song object to create
+     * @return the created song object
+     */
+    @PostMapping
+    public Book createSong(@RequestBody Book song) {
+        return bookService.createBook(song);
+    }
+
+    /**
+     * Updates an existing song and returns it.
+     *
+     * @param song the song object to update
+     * @return the updated song object
+     */
+    @PutMapping
+    public Book updateSong(@RequestBody Book song) {
+        return bookService.updateBook(song);
+    }
+
+    /**
+     * Deletes a song by its id.
+     *
+     * @param id the id of the song to delete
+     */
+    @DeleteMapping("/{id}")
+    public void deleteSongById(@PathVariable Long id) {
+        bookService.deleteBookById(id);
     }
 }
